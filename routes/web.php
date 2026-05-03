ffbbf6a6968a5fe4c335337df61e3643c59c689a
bakunzifiston <82505728+bakunzifiston@users.noreply.github.com>
2026-05-03 17:34:34 +0200

    seedss

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return Auth::check()
         ? redirect()->route('dashboard')
-        : view('welcome');
+        : redirect()->route('login');
 });
 
 Route::middleware('guest')->group(function (): void {
